c6c14c13806fba6b78e1ec2ffce61393b75b35e8
kanimum <tikuwakunn@icloud.com>
2022-10-21 08:31:19 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -55,12 +55,12 @@ impl<T: Clone> Matrix<T> {
 impl<T: Clone + Add<Output = T>> Add for Matrix<T> {
     type Output = Option<Self>;
 
-    fn add(self, other: Self) -> Self::Output {
-        if self.rows != other.rows || self.cols != other.cols {
+    fn add(self, rhs: Self) -> Self::Output {
+        if self.rows != rhs.rows || self.cols != rhs.cols {
             None
         } else {
             let mut v = Vec::new();
-            for (a, b) in self.values.iter().zip(other.values.iter()) {
+            for (a, b) in self.values.iter().zip(rhs.values.iter()) {
                 v.push(a.clone() + b.clone());
             }
             Some(Matrix {
